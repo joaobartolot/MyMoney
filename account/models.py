e7adde7b61771736a5856354c5b01d2e9764bede
Joao Victor Bartolot <jvcbartolot@gmail.com>
2019-11-16 01:29:36 -0300

    Refining the account table

--- account/models.py
+++ account/models.py
@@ -3,23 +3,35 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Account_Type(models.Model):
-    name = models.TextField(max_length=20)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.name}'
+
+    class Meta:
+        db_table = "account_type"
 
 class Bank(models.Model):
-    name = models.TextField(max_length=20)
+    name = models.CharField(max_length=20)
+    rate = models.FloatField(null=True)
+    income = models.FloatField(null=True)
 
     def __str__(self):
         return f'{self.name}'
 
+    class Meta:
+        db_table = "bank"
+
 class Account(models.Model):
-    name = models.TextField(max_length=20)
+    name = models.CharField(max_length=20)
     balance = models.IntegerField()
     initial_balance = models.IntegerField()
+    salary = models.FloatField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     account_type = models.ForeignKey(Account_Type, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}({self.balance}), user: {self.user}'
+
+    class Meta:
+        db_table = "account"
