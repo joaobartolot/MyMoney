e92567f28e17c932ed422a257e996b283b54d1a2
Joao Bartolot (linux) <jvcbartolot@gmail.com>
2019-11-24 03:01:55 -0300

    Account list were created

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, account_list
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('accounts', account_list, name='account-list')
 ]
--- account/views.py
+++ account/views.py
@@ -6,7 +6,13 @@ from .models import Account
 def home_view(request):
     context = {
         'title': 'Home',
-        'accounts': Account.objects.all().filter(user=request.user.id)
     }
 
     return render(request, template_name='account/home.html', context=context)
+
+def account_list(request):
+    context = {
+        'title': 'Accounts'
+    }
+
+    return render(request, template_name='account/account_list.html', context=context)
